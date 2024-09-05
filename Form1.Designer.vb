2bed751de207244cf43b85b54f83a60b21e26072
Ammar Hammad <amargamal832909@gmail.com>
2024-09-05 11:29:16 +0300

    learn about selection with if statement

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,137 +23,11 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.btnGo = New System.Windows.Forms.Button()
-        Me.btnVariables = New System.Windows.Forms.Button()
-        Me.btnDataTypes = New System.Windows.Forms.Button()
-        Me.btnInput = New System.Windows.Forms.Button()
-        Me.txtFirstName = New System.Windows.Forms.TextBox()
-        Me.lblFirstName = New System.Windows.Forms.Label()
-        Me.txtLastName = New System.Windows.Forms.TextBox()
-        Me.lblLastName = New System.Windows.Forms.Label()
-        Me.txtGender = New System.Windows.Forms.TextBox()
-        Me.lblGener = New System.Windows.Forms.Label()
-        Me.lstOccupation = New System.Windows.Forms.ListBox()
-        Me.lblOccupation = New System.Windows.Forms.Label()
-        Me.lblNumber1 = New System.Windows.Forms.Label()
         Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
-        Me.txtNumber1 = New System.Windows.Forms.TextBox()
-        Me.txtNumber2 = New System.Windows.Forms.TextBox()
-        Me.lblNumber2 = New System.Windows.Forms.Label()
-        Me.btnSum = New System.Windows.Forms.Button()
+        Me.lblCountry = New System.Windows.Forms.Label()
+        Me.txtCountry = New System.Windows.Forms.TextBox()
+        Me.btnGreeting = New System.Windows.Forms.Button()
         Me.SuspendLayout()
-        '
-        'btnGo
-        '
-        Me.btnGo.Location = New System.Drawing.Point(145, 46)
-        Me.btnGo.Name = "btnGo"
-        Me.btnGo.Size = New System.Drawing.Size(154, 84)
-        Me.btnGo.TabIndex = 0
-        Me.btnGo.Text = "Go"
-        Me.btnGo.UseVisualStyleBackColor = True
-        '
-        'btnVariables
-        '
-        Me.btnVariables.Location = New System.Drawing.Point(364, 46)
-        Me.btnVariables.Name = "btnVariables"
-        Me.btnVariables.Size = New System.Drawing.Size(167, 88)
-        Me.btnVariables.TabIndex = 1
-        Me.btnVariables.Text = "Variable Demonstration"
-        Me.btnVariables.UseVisualStyleBackColor = True
-        '
-        'btnDataTypes
-        '
-        Me.btnDataTypes.Location = New System.Drawing.Point(145, 170)
-        Me.btnDataTypes.Name = "btnDataTypes"
-        Me.btnDataTypes.Size = New System.Drawing.Size(154, 93)
-        Me.btnDataTypes.TabIndex = 2
-        Me.btnDataTypes.Text = "Data Types"
-        Me.btnDataTypes.UseVisualStyleBackColor = True
-        '
-        'btnInput
-        '
-        Me.btnInput.Location = New System.Drawing.Point(364, 170)
-        Me.btnInput.Name = "btnInput"
-        Me.btnInput.Size = New System.Drawing.Size(152, 93)
-        Me.btnInput.TabIndex = 3
-        Me.btnInput.Text = "Input"
-        Me.btnInput.UseVisualStyleBackColor = True
-        '
-        'txtFirstName
-        '
-        Me.txtFirstName.Location = New System.Drawing.Point(223, 307)
-        Me.txtFirstName.Name = "txtFirstName"
-        Me.txtFirstName.Size = New System.Drawing.Size(233, 26)
-        Me.txtFirstName.TabIndex = 4
-        '
-        'lblFirstName
-        '
-        Me.lblFirstName.AutoSize = True
-        Me.lblFirstName.Location = New System.Drawing.Point(88, 313)
-        Me.lblFirstName.Name = "lblFirstName"
-        Me.lblFirstName.Size = New System.Drawing.Size(86, 20)
-        Me.lblFirstName.TabIndex = 5
-        Me.lblFirstName.Text = "First Name"
-        '
-        'txtLastName
-        '
-        Me.txtLastName.Location = New System.Drawing.Point(223, 356)
-        Me.txtLastName.Name = "txtLastName"
-        Me.txtLastName.Size = New System.Drawing.Size(233, 26)
-        Me.txtLastName.TabIndex = 6
-        '
-        'lblLastName
-        '
-        Me.lblLastName.AutoSize = True
-        Me.lblLastName.Location = New System.Drawing.Point(88, 361)
-        Me.lblLastName.Name = "lblLastName"
-        Me.lblLastName.Size = New System.Drawing.Size(86, 20)
-        Me.lblLastName.TabIndex = 7
-        Me.lblLastName.Text = "Last Name"
-        '
-        'txtGender
-        '
-        Me.txtGender.Location = New System.Drawing.Point(223, 409)
-        Me.txtGender.Name = "txtGender"
-        Me.txtGender.Size = New System.Drawing.Size(233, 26)
-        Me.txtGender.TabIndex = 8
-        '
-        'lblGener
-        '
-        Me.lblGener.AutoSize = True
-        Me.lblGener.Location = New System.Drawing.Point(92, 415)
-        Me.lblGener.Name = "lblGener"
-        Me.lblGener.Size = New System.Drawing.Size(63, 20)
-        Me.lblGener.TabIndex = 9
-        Me.lblGener.Text = "Gender"
-        '
-        'lstOccupation
-        '
-        Me.lstOccupation.FormattingEnabled = True
-        Me.lstOccupation.ItemHeight = 20
-        Me.lstOccupation.Items.AddRange(New Object() {"Doctor", "Programmer", "Teacher", "Student", "Tinker", "Tailor"})
-        Me.lstOccupation.Location = New System.Drawing.Point(588, 69)
-        Me.lstOccupation.Name = "lstOccupation"
-        Me.lstOccupation.Size = New System.Drawing.Size(169, 184)
-        Me.lstOccupation.TabIndex = 10
-        '
-        'lblOccupation
-        '
-        Me.lblOccupation.AutoSize = True
-        Me.lblOccupation.Location = New System.Drawing.Point(584, 46)
-        Me.lblOccupation.Name = "lblOccupation"
-        Me.lblOccupation.Size = New System.Drawing.Size(90, 20)
-        Me.lblOccupation.TabIndex = 11
-        Me.lblOccupation.Text = "Occupation"
-        '
-        'lblNumber1
-        '
-        Me.lblNumber1.AutoSize = True
-        Me.lblNumber1.Location = New System.Drawing.Point(92, 501)
-        Me.lblNumber1.Name = "lblNumber1"
-        Me.lblNumber1.Size = New System.Drawing.Size(78, 20)
-        Me.lblNumber1.TabIndex = 12
-        Me.lblNumber1.Text = "Number 1"
         '
         'ContextMenuStrip1
         '
@@ -161,83 +35,47 @@ Partial Class Form1
         Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
         Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
         '
-        'txtNumber1
+        'lblCountry
         '
-        Me.txtNumber1.Location = New System.Drawing.Point(223, 494)
-        Me.txtNumber1.Name = "txtNumber1"
-        Me.txtNumber1.Size = New System.Drawing.Size(233, 26)
-        Me.txtNumber1.TabIndex = 14
+        Me.lblCountry.AutoSize = True
+        Me.lblCountry.Location = New System.Drawing.Point(87, 229)
+        Me.lblCountry.Name = "lblCountry"
+        Me.lblCountry.Size = New System.Drawing.Size(213, 20)
+        Me.lblCountry.TabIndex = 14
+        Me.lblCountry.Text = "What country your are from ?"
         '
-        'txtNumber2
+        'txtCountry
         '
-        Me.txtNumber2.Location = New System.Drawing.Point(223, 556)
-        Me.txtNumber2.Name = "txtNumber2"
-        Me.txtNumber2.Size = New System.Drawing.Size(233, 26)
-        Me.txtNumber2.TabIndex = 15
+        Me.txtCountry.Location = New System.Drawing.Point(322, 229)
+        Me.txtCountry.Name = "txtCountry"
+        Me.txtCountry.Size = New System.Drawing.Size(134, 26)
+        Me.txtCountry.TabIndex = 15
         '
-        'lblNumber2
+        'btnGreeting
         '
-        Me.lblNumber2.AutoSize = True
-        Me.lblNumber2.Location = New System.Drawing.Point(92, 561)
-        Me.lblNumber2.Name = "lblNumber2"
-        Me.lblNumber2.Size = New System.Drawing.Size(78, 20)
-        Me.lblNumber2.TabIndex = 16
-        Me.lblNumber2.Text = "Number 2"
-        '
-        'btnSum
-        '
-        Me.btnSum.Location = New System.Drawing.Point(278, 604)
-        Me.btnSum.Name = "btnSum"
-        Me.btnSum.Size = New System.Drawing.Size(140, 49)
-        Me.btnSum.TabIndex = 17
-        Me.btnSum.Text = "Sum"
-        Me.btnSum.UseVisualStyleBackColor = True
+        Me.btnGreeting.Location = New System.Drawing.Point(322, 275)
+        Me.btnGreeting.Name = "btnGreeting"
+        Me.btnGreeting.Size = New System.Drawing.Size(134, 47)
+        Me.btnGreeting.TabIndex = 16
+        Me.btnGreeting.Text = "Greeting!"
+        Me.btnGreeting.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 682)
-        Me.Controls.Add(Me.btnSum)
-        Me.Controls.Add(Me.lblNumber2)
-        Me.Controls.Add(Me.txtNumber2)
-        Me.Controls.Add(Me.txtNumber1)
-        Me.Controls.Add(Me.lblNumber1)
-        Me.Controls.Add(Me.lblOccupation)
-        Me.Controls.Add(Me.lstOccupation)
-        Me.Controls.Add(Me.lblGener)
-        Me.Controls.Add(Me.txtGender)
-        Me.Controls.Add(Me.lblLastName)
-        Me.Controls.Add(Me.txtLastName)
-        Me.Controls.Add(Me.lblFirstName)
-        Me.Controls.Add(Me.txtFirstName)
-        Me.Controls.Add(Me.btnInput)
-        Me.Controls.Add(Me.btnDataTypes)
-        Me.Controls.Add(Me.btnVariables)
-        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.btnGreeting)
+        Me.Controls.Add(Me.txtCountry)
+        Me.Controls.Add(Me.lblCountry)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents btnGo As Button
-    Friend WithEvents btnVariables As Button
-    Friend WithEvents btnDataTypes As Button
-    Friend WithEvents btnInput As Button
-    Friend WithEvents txtFirstName As TextBox
-    Friend WithEvents lblFirstName As Label
-    Friend WithEvents txtLastName As TextBox
-    Friend WithEvents lblLastName As Label
-    Friend WithEvents txtGender As TextBox
-    Friend WithEvents lblGener As Label
-    Friend WithEvents lstOccupation As ListBox
-    Friend WithEvents lblOccupation As Label
-    Friend WithEvents lblNumber1 As Label
     Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
-    Friend WithEvents txtNumber1 As TextBox
-    Friend WithEvents txtNumber2 As TextBox
-    Friend WithEvents lblNumber2 As Label
-    Friend WithEvents btnSum As Button
+    Friend WithEvents lblCountry As Label
+    Friend WithEvents txtCountry As TextBox
+    Friend WithEvents btnGreeting As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,127 +1,30 @@
 ﻿Public Class Form1
 
 
-    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
 
-        MsgBox("Welcome to the VB.NET programming course")
+    Private Sub btnGreeting_Click(sender As Object, e As EventArgs) Handles btnGreeting.Click
 
-        MessageBox.Show("Another way to display words")
+        Dim stCountry As String
 
-    End Sub
+        stCountry = txtCountry.Text
 
+        If stCountry.ToUpper = "EGYPT" Then
+            MsgBox("Hello to Egyptions")
+            MsgBox("you are welcome")
+        ElseIf stCountry.ToUpper = "Greek" Then
+            MsgBox("u r from Greece")
+            MsgBox("welcome bro")
+        Else
+            MsgBox("hello anonymouse")
+            MsgBox("I hope u r well")
+        End If
 
-    Private Sub btnVariables_Click(sender As Object, e As EventArgs) Handles btnVariables.Click
-
-        Dim stFirstName As String
-        Dim stLastName As String
-
-
-        stFirstName = "Ammar"
-        stLastName = "Hammad"
-
-        MsgBox("hello and welcome Mr. " & stFirstName & " " & stLastName & " I hope u r well")
-
-        stFirstName = "Kareem"
-        stLastName = "Gamal"
-
-
-        MsgBox("hello and welcome Mr. " & stFirstName & " " & stLastName & " I hope u r well")
-
-
-
-    End Sub
-
-
-
-    Private Sub btnDataTypes_Click(sender As Object, e As EventArgs) Handles btnDataTypes.Click
-
-        Dim stMake As String
-        Dim stModel As String
-        Dim iDoors As Integer
-        Dim stColor As String
-        Dim bTaxed As Boolean
-        Dim iEngineSize As Integer
-        Dim decPrice As Decimal
-        Dim dtDataRegistered As Date
-
-        stMake = "Ford"
-        stModel = "Escort"
-        iDoors = 5
-        stColor = "Blue"
-        bTaxed = True
-        iEngineSize = 1200
-        decPrice = 9999.99
-        dtDataRegistered = #11/20/2024#
-
-        MsgBox("the car is " & stMake & vbNewLine & stModel & vbNewLine &
-               iDoors & vbNewLine & stColor & vbNewLine & iEngineSize &
-               vbNewLine & decPrice & vbNewLine & dtDataRegistered)
-
+        MsgBox("You Entered " & stCountry)
 
 
 
 
     End Sub
 
-    Private Sub btnInput_Click(sender As Object, e As EventArgs) Handles btnInput.Click
 
-        Dim stFirstName, stLastName, stGender, stOccupation As String
-
-
-        stFirstName = txtFirstName.Text
-        stLastName = txtLastName.Text
-        stGender = txtGender.Text
-
-        stOccupation = lstOccupation.SelectedItem
-
-        MsgBox("Hello Mr. " & stFirstName & " " & stLastName &
-               " you are a " & stGender & " " & stOccupation)
-
-    End Sub
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-        lstOccupation.Items.Add("Writer")
-        lstOccupation.Items.Add("Actor")
-        lstOccupation.Items.Add("Painter")
-        lstOccupation.Items.Add("Artist")
-
-
-    End Sub
-
-    Private Sub btnSum_Click(sender As Object, e As EventArgs) Handles btnSum.Click
-
-        Dim dblNumber1, dblNumber2, dblResult As Double
-
-        dblNumber1 = txtNumber1.Text
-        dblNumber2 = txtNumber2.Text
-
-
-        dblResult = dblNumber1 + dblNumber2
-        MsgBox("Sum = " & dblResult)
-
-        dblResult = dblNumber1 - dblNumber2
-        MsgBox("Subtraction = " & dblResult)
-
-        dblResult = dblNumber1 * dblNumber2
-        MsgBox("Multiplication = " & dblResult)
-
-        dblResult = dblNumber1 / dblNumber2
-        MsgBox("Division = " & dblResult)
-
-        dblResult = dblNumber1 ^ dblNumber2
-        MsgBox("Number 1 Power of Number 2 = " & dblResult)
-
-
-
-
-        ' '\' this to divide and ignore the reminder
-        dblResult = dblNumber1 \ dblNumber2
-        MsgBox("division ignore reminder = " & dblResult)
-
-        dblResult = dblNumber1 Mod dblNumber2
-        MsgBox("Num1 Mod Num2 = " & dblResult)
-
-
-    End Sub
 End Class
