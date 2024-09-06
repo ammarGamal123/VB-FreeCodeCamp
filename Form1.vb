c470d5c3e54d88644ad2a43744039f0e01f9ae5a
Ammar Hammad <amargamal832909@gmail.com>
2024-09-06 16:45:53 +0300

    learn two dimension array and how to iterate inside it

--- Form1.vb
+++ Form1.vb
@@ -1,19 +1,24 @@
 ﻿Public Class Form1
 
     Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        Dim astPeople(4, 5) As String
 
-        Dim astFruits(4) As String
+        astPeople(0, 0) = "Barack"
+        astPeople(0, 1) = "Obama"
+        astPeople(1, 1) = "Male"
+        astPeople(1, 2) = "American"
+        astPeople(2, 2) = "President"
 
-        astFruits(0) = "Apple"
-        astFruits(1) = "Banana"
-        astFruits(2) = "Orange"
-        astFruits(3) = "Pineapple"
-        astFruits(4) = "Mango"
+        Dim x, y As Integer
 
-        Dim iCount As Integer
-        For iCount = 0 To astFruits.Length - 1
-            MsgBox(astFruits(iCount))
-        Next
+        ' Loop over the first dimension (rows) and second dimension (columns)
+        For x = 0 To astPeople.GetLength(0) - 1 ' Loop over rows
+            For y = 0 To astPeople.GetLength(1) - 1 ' Loop over columns
+                If Not String.IsNullOrEmpty(astPeople(x, y)) Then
+                    MsgBox("astPeople(" & x & ", " & y & ") = " & astPeople(x, y))
+                End If
+            Next y
+        Next x
 
     End Sub
 
